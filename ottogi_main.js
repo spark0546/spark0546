878a4ad0d696e5f872e2c658be0c1d69cf5645d5
spark0546 <65319413+spark0546@users.noreply.github.com>
2023-12-28 12:59:44 +0900

    Update ottogi_main.js
    1228 edit

--- ottogi_main.js
+++ ottogi_main.js
@@ -1,1 +1,22 @@
+let slideEa = $('.slides').children().length;
+$('.last_page').text(slideEa.toString().padStart(2,'0'));
+page();
 
+let visual = $('.slides').bxSlider({
+    controls:false,
+    auto:true,
+    onSlideAfter:function(){
+        page();
+    }
+});
+$('.main_prev').click(function(){
+    visual.goToPrevSlide();
+});
+$('.main_next').click(function(){
+    visual.goToNextSlide();
+});
+function page(){
+    let currentNum = $('#visual .active').text();
+    $('.current_page').text(currentNum.toString().padStart(2,'0'));
+    $('.page_width').css('width',100 * currentNum / slideEa + '%');
+}
